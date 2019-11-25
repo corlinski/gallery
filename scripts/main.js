13ff8836b9d3f3facf12d7a4f60f54da4d0b7739
corlinski <cezary.orlinski@yahoo.com>
2019-11-25 17:08:25 +0100

    Wyswietl komunikat o pustej galerii

--- scripts/main.js
+++ scripts/main.js
@@ -9,24 +9,18 @@ function renderPhoto(url) {
 }
 
 
-function displayPhotos() {
+function displayPhotos(photos) {
     console.log("displayPhotos()");
 
-    const photos = [
-        "https://picsum.photos/350/200?random=1",
-        "https://picsum.photos/350/200?random=2",
-        "https://picsum.photos/350/200?random=3"
-    ];
-
-    for (let i of photos) {
+    // for (let i of photos) {
         //renderPhoto(i);
         // renderPhoto(photos[1]);
         // renderPhoto(photos[2]);
-    }
+    // }
     
-    for (let i = 0; i < 3; i++) {
-         renderPhoto("https://picsum.photos/350/200?random=" + i);
-    }
+    // for (let i = 0; i < 3; i++) {
+    //      renderPhoto("https://picsum.photos/350/200?random=" + i);
+    // }
 
     // photos.forEach(
     //     // callback synchroniczny -> wywolanie zwrotne
@@ -41,10 +35,39 @@ function displayPhotos() {
 }
 
 
+function displayMessage(text, cssClass) {
+        console.log("displayMessage()");
+        // 1. Tworzenie elementu html 
+        const $text = document.createElement('p');
+        //$text.innerHTML = text  - tutaj parsuje HTML
+        //$text.innerText = text  - nie  parsuje HTML przy odczycie
+        $text.textContent = text;
+        $text.classList.add('alert', cssClass);     // dodanie klas 
+        // 2. Dodanie "dziecka" (child) - elementu do juz istniejacego "rodzica" - parenta
+        const $main = document.querySelector('main');
+        $main.appendChild($text);    
+}
 
 function main() {
     console.log("main()");
-    displayPhotos();
+
+    const photos = [
+        "https://picsum.photos/350/200?random=1",
+        "https://picsum.photos/350/200?random=2",
+        "https://picsum.photos/350/200?random=3"
+    ];
+
+    if (photos.length > 0) {
+        displayPhotos(photos);
+    } else {
+        displayMessage("Nie ma zdjęć w galerii", 'alert-danger');
+    };
+
+    // const ONE_SECOND = 1000;
+    // setTimeout(function () {
+    //     displayMessage("Tekst to wyswietlenia", 'alert-danger');
+    // }, 2 * ONE_SECOND);
+
 }
 
 console.log(1);
