6b0ee10469d3efbabdb97bce1f75b4216d7bc96e
corlinski <cezary.orlinski@yahoo.com>
2019-11-27 15:22:49 +0100

    Cookies 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -27,21 +27,21 @@ function displayMessage(displayText) {
 async function main() {
     const url = 'data/photos.json';
     const displayText = 'UWAGA TEXT';
+    loader.show(); 
 
     // eslint-disable-next-line no-undef
     let photos = storage.read('fotki');
 
     if (!photos) {   
-        loader.show();
         const photos1 = await makeRequest(url);
         const photos2 = await makeRequest(url);
         const photos3 = await makeRequest(url);
         photos = [].concat(photos1, photos2, photos3);
     
         storage.save('fotki', photos);  // zapisz zbior
-    
-        loader.hide();
+        //loader.hide(); 
     }        
+
 
     photos.forEach(function (photo) {
         renderPhoto(photo.imageUrl, photo.name);
